78a42babcf063e5495f00fed2b787b16e3f9954f
github username <github email address>
2020-05-04 22:41:22 -0700

    Edit route and form  created. Users are only able to see and click the link to edit if they are viewing their own profile. Users are not able to edit othe user's lifts.

--- app/controllers/user_lifts_controller.rb
+++ app/controllers/user_lifts_controller.rb
@@ -6,35 +6,44 @@ class UserLiftsController < ApplicationController
         erb :'user_lifts/new'
     end
 
+    get '/user-lifts/:id/edit' do
+        @user_lift = UserLift.find(params[:id])
+        @lift_types = LiftType.all
+        if current_user == @user_lift.user
+            erb :'user_lifts/edit.html'
+        else
+            redirect "/users"
+        end
+    end
+
     post '/user-lifts' do
         
-        check_lift_params(params)
-        weight_input_check(params[:weight])
+        check_params(params, "user-lifts/new")
+        weight_input_check(params[:weight], "user-lifts/new")
+        if !params.has_key?(:lift_type)
+            set_session_message("An option needs to be selected for the lift type")
+            redirect "/user-lifts/new"
+        end
 
         UserLift.create(:weight => params[:weight], :lift_type_id => params[:lift_type], :user_id => current_user.id)
         redirect "/users/#{current_user.slug}"
     end
 
+    patch "/user-lifts/:id/edit" do
+        
+    end
+
     helpers do
-        def check_lift_params(params)
-            params.each do |k, v|
-                if v.empty?
-                    deslugged_key = deslug_key(key)
-                    set_session_message("#{deslugged_key} cannot be left empty")
-                    redirect "/user-lifts/new"
-                end
-            end
-        end
-        def delug_key(key)
+        def deslug_key(key)
             key_array = key.split("_")
             key_array.join(" ")
         end
     end
 
-    def weight_input_check(weight_value)
+    def weight_input_check(weight_value, current_page)
         if weight_value.to_i < 0
             set_session_message("The value of weight must be a positive integer.")
-            redirect "/user-lifts/new"
+            redirect "/#{current_page}"
         end
     end
 
